5278dd755cfb88a29608a47f005ac6c466a10d72
Cynthiachelangat <cchela789@gmail.com>
2024-06-12 08:46:46 +0300

    auth login and logout

--- routes/auth.py
+++ routes/auth.py
@@ -13,3 +13,29 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data)
+        user = User(username=form.username.data, email = form.email.data, password=hashed_password, address = form.address.data)
+        db.session.add(user)
+        db.session.commit
+        login_user(user)
+        return redirect(url_for('main.home'))
+    return render_template('register.html', title='Register', form=form)
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user, remember = form.remember.data)
+            return redirect(url_for('main.home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title = 'login', form = form)
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
